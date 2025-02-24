c60f123eb35b9404312236d63cc96afe8df4de9a
MatiasZubieta <matias.zubieta4015@gmail.com>
2025-02-23 21:11:05 -0300

    se agrego codigo para mostrar y conectar a la base de datos

--- conexion.php
+++ conexion.php
@@ -1,16 +1,14 @@
 <?php
 
 $server = "localhost";
-$user = "root"
+$user = "root";
 $pass = "";
 $db = "proyecto";
 
-$conexion = new mysql($server,$user,$pass,$db);
+$conexion = mysqli_connect ($server, $user, $pass, $db);
 
 if ($conexion->connect_error) {
     die("conexion fallida" . $conexion->connect_error);
-} else {
-    echo "conectado";
 }
 
 $sql = "SELECT ID, nombre, DNI FROM personas";
