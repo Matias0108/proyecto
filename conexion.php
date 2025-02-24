1b15afd699647301d141adb00fe965aea45553c0
MatiasZubieta <matias.zubieta4015@gmail.com>
2025-02-23 21:10:32 -0300

    se agrego codigo para mostrar y conectar a la base de datos

---
+++ conexion.php
@@ -0,0 +1,38 @@
+<?php
+
+$server = "localhost";
+$user = "root"
+$pass = "";
+$db = "proyecto";
+
+$conexion = new mysql($server,$user,$pass,$db);
+
+if ($conexion->connect_error) {
+    die("conexion fallida" . $conexion->connect_error);
+} else {
+    echo "conectado";
+}
+
+$sql = "SELECT ID, nombre, DNI FROM personas";
+$resultado = $conexion->query($sql);
+
+if ($resultado->num_rows > 0) {
+    echo "<table border='1'>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>DNI</th>
+            </tr>";
+    while($row = $resultado->fetch_assoc()) {
+        echo "<tr>
+                <td>".$row["ID"]."</td>
+                <td>".$row["nombre"]."</td>
+                <td>".$row["DNI"]."</td>
+              </tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 resultados";
+}
+
+?>
